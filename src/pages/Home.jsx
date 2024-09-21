742a96fbce70592f4b2c90a7952760e4a621bc6d
mohamuhsin <muhsinohoho@outlook.com>
2024-09-21 23:02:34 +0300

    navigating btn pages with Links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return <h1>My Home Page</h1>;
+    return (
+        <>
+            <h1>My Home Page</h1>
+            <p>
+                Go to <Link to="/products">The List of products</Link>
+            </p>
+        </>
+    );
 }
